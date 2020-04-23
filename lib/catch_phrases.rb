ba008c2e742f7d1b7f4a0e8a72f83dc595aa1807
breezy-desk-5482 <rmcclorey1@gmail.com>
2020-04-23 00:33:44 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,14 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  danger ="It's Dangerous To Go Alone! Take This."
+  puts danger
+end
+
+def all_phrases
+  mario
+  toadstool
+  link
+end
